c3a06cc761ed560aec542c516b6018f70de5a519
[Samir Shrestha] <[linkwithsamir@gmail.com]>
2023-12-13 14:15:25 +0545

    update: assets relocation

--- JS/pixel_adventure/js/movement.js
+++ JS/pixel_adventure/js/movement.js
@@ -30,8 +30,7 @@ function spriteSlider(object) {
 
 function moveRight(object) {
   if (keys.right.pressed) {
-    object.spriteImg.src =
-      '../assets/pixel_adventure_1/main_characters/virtual_guy/run-right.png'
+    object.spriteImg.src = '../assets/main_characters/virtual_guy/run-right.png'
     object.velocity.x = 5
     object.totalFrame = 12
     object.lastKey = 'right'
@@ -41,7 +40,7 @@ function moveRight(object) {
     object.isGrounded
   ) {
     object.spriteImg.src =
-      '../assets/pixel_adventure_1/main_characters/virtual_guy/idle-right.png'
+      '../assets/main_characters/virtual_guy/idle-right.png'
     object.velocity.x = 0
     object.totalFrame = 11
   }
@@ -49,14 +48,12 @@ function moveRight(object) {
 
 function moveLeft(object) {
   if (keys.left.pressed) {
-    object.spriteImg.src =
-      '../assets/pixel_adventure_1/main_characters/virtual_guy/run-left.png'
+    object.spriteImg.src = '../assets/main_characters/virtual_guy/run-left.png'
     object.velocity.x = -5
     object.totalFrame = 12
     object.lastKey = 'left'
   } else if (object.lastKey == 'left' && !keys.left.pressed) {
-    object.spriteImg.src =
-      '../assets/pixel_adventure_1/main_characters/virtual_guy/idle-left.png'
+    object.spriteImg.src = '../assets/main_characters/virtual_guy/idle-left.png'
     object.velocity.x = 0
     object.totalFrame = 11
   }
@@ -65,7 +62,7 @@ function moveLeft(object) {
 function moveJump(object) {
   if (keys.jump.pressed && object.isGrounded) {
     object.spriteImg.src =
-      '../assets/pixel_adventure_1/main_characters/virtual_guy/jump-right.png'
+      '../assets/main_characters/virtual_guy/jump-right.png'
     object.velocity.y = -20
     object.totalFrame = 1
     object.isGrounded = false
@@ -73,27 +70,25 @@ function moveJump(object) {
 }
 
 function slideRight(object) {
-     // console.log(object.isGrounded)
+  // console.log(object.isGrounded)
   if (!object.isGrounded) {
     object.spriteImg.src =
-      '../assets/pixel_adventure_1/main_characters/virtual_guy/slide-right.png'
-     //  console.log(object.velocity.y);
-      object.velocity.y = 1
-      object.totalFrame = 5
-      if (keys.jump.pressed) {
-          object.velocity.y = -15
-          object.velocity.x = -5
-          
-     }
+      '../assets/main_characters/virtual_guy/slide-right.png'
+    //  console.log(object.velocity.y);
+    object.velocity.y = 1
+    object.totalFrame = 5
+    if (keys.jump.pressed) {
+      object.velocity.y = -15
+      object.velocity.x = -5
+    }
   }
   object.draw()
 }
 
 function slideLeft(object) {
   if (!object.isGrounded) {
-     console.log('hadfha')
     object.spriteImg.src =
-      '../assets/pixel_adventure_1/main_characters/virtual_guy/slide-left.png'
+      '../assets/main_characters/virtual_guy/slide-left.png'
     object.velocity.y = 1
     object.totalFrame = 5
     if (keys.jump.pressed) {
